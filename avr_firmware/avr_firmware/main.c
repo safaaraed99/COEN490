aec1778f831b05b3fe8038411f637c930df812f2
Matthew Faigan <matthew.faigan@gmail.com>
2024-03-10 21:21:52 -0400

    Copy over GPIO config from earlier test

--- avr_firmware/avr_firmware/main.c
+++ avr_firmware/avr_firmware/main.c
@@ -7,12 +7,33 @@
 
 #include <avr/io.h>
 
+void setup_gpio(void);
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	// SETUP
+	setup_gpio();
+	
+	// LOOP
+	while (1)
+	{
+	}
+}
+
+void setup_gpio(void)
+{
+	// PORTxn : If port x, pin n is input: 1 enables internal pull-up. If port x, pin n is output: sets value of port.
+	// DDxn : Data Direction, Port x, Pin n. 1 = Output, 0 = Input
+	PORTB = (1<<PORTB7) | (1<<PORTB6);
+	DDRB = (1<<DDB0);
+	
+	PORTC = (1<<PORTC5) | (1<<PORTC3) | (1<<PORTC2);
+	DDRC = (1<<DDC5) | (1<<DDC4) | (1<<DDC3) | (1<<DDC2);
+	
+	PORTD = (1<<PORTD4);
+	DDRD = (1<<DDD7) | (1<<DDD2);
+	
+	PORTE = (1<<PORTE2) | (1<<PORTE1) | (1<<PORTE0);
+	DDRE = (1<<DDE2);
 }
 
